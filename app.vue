f9e1a6f5e3f60be7b32a1a23495718529d31c338
Fran Blanco <arbust908@gmail.com>
2022-08-29 09:30:20 -0300

    Refract Init

---
+++ app.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+useHead({
+  title: 'Vitesse Nuxt 3',
+  link: [
+    {
+      rel: 'icon', type: 'image/png', href: '/nuxt.png',
+    },
+  ],
+})
+
+const documentHeight = () => {
+  // https://nirazanbasnet.medium.com/dont-use-100vh-for-mobile-responsive-43a709c7e9e6
+  const doc = document.documentElement
+  doc.style.setProperty('--doc-height', `${window.innerHeight}px`)
+}
+onMounted(() => {
+  if (document)
+    documentHeight()
+
+  if (window)
+    window.addEventListener('resize', documentHeight)
+})
+</script>
+
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<style>
+:root {
+ --doc-height: 100%;
+}
+html, body , #__nuxt{
+  margin: 0;
+  padding: 0;
+  height: 100vh; /* fallback for Js load */
+  height: var(--doc-height);
+}
+
+html.dark {
+  background: #222;
+  color: white;
+}
+</style>
---
+++ prisma/migrations/20220821125121_init/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NULL,
+    `emailVerified` DATETIME(3) NULL,
+    `image` VARCHAR(191) NULL,
+    `title` VARCHAR(191) NULL,
+    `role` ENUM('USER', 'ADMIN') NOT NULL DEFAULT 'USER',
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(255) NOT NULL,
+    `content` TEXT NOT NULL,
+    `contentHtml` TEXT NOT NULL,
+    `hidden` BOOLEAN NOT NULL DEFAULT false,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `authorId` VARCHAR(191) NOT NULL,
+
+    INDEX `Post_authorId_idx`(`authorId`),
+    FULLTEXT INDEX `Post_title_content_idx`(`title`, `content`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
